291c4d2b7ffb4998119c3039ef6297c3cb9ad7bd
Alex <adasneves@student.bridgew.edu>
2022-10-18 08:27:58 -0400

    initial Commit

---
+++ 10-18.py
@@ -0,0 +1,12 @@
+top_us_cities_by_population = {
+    'Chicago':2_696_555,
+    "Dallas":1_288_457,
+    "Houston":2_288_250,
+    "Los Angeles":3_849_297,
+    "New York":  8_467_513,
+    "Philadelphia" : 1_576_251,
+    "Phoenix": 1_624_569,
+    "San Antonio": 1_451_853,
+    "San Diego": 1_381_611,
+    "San Jose": 983_489
+}
